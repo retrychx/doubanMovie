d8872688bc01270c36e4bed2ceb9dd7de2a7f05c
retrychx <retrychx@163.com>
2019-11-14 17:38:41 +0800

    searchbar

--- lib/pages/hot_movie.dart
+++ lib/pages/hot_movie.dart
@@ -1,17 +1,33 @@
+import 'package:douban/widgets/search_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:douban/widgets/app_bar_container.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HotMovie extends StatefulWidget {
   @override
   HotMovieState createState() => HotMovieState();
 }
 
-class HotMovieState extends State<HotMovie> {
+class HotMovieState extends State<HotMovie> with TickerProviderStateMixin {
+  TabController tabController;
   @override
   Widget build(BuildContext context) {
+    tabController = TabController(length: 2, initialIndex: 0, vsync: this);
     return Scaffold(
-      appBar: new AppBar(
-        title: Text("热映"),
+        appBar: AppBarContainer(
+      height: ScreenUtil.instance.setHeight(168),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+                left: ScreenUtil.instance.setWidth(25),
+                right: ScreenUtil.instance.setWidth(25)),
+            width: double.infinity,
+            height: ScreenUtil.instance.setHeight(100),
+            child: SearchWidget(),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
---
+++ lib/widgets/app_bar_container.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+///使用传进来的控件作为标题
+class AppBarContainer extends StatelessWidget implements PreferredSizeWidget {
+  AppBarContainer(
+      {@required this.child,
+      this.height = 0,
+      this.backgroundColor = Colors.transparent});
+
+  final Widget child;
+  final double height;
+  final Color backgroundColor;
+
+  @override
+  Size get preferredSize {
+    //设置标题栏高度，一般app中的标题栏高度是统一的
+    return Size.fromHeight(height);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //如果需要添加标题右侧按钮或者右侧副标题可添加进来
+    List<Widget> children = <Widget>[];
+    if (child != null) {
+      children.add(child);
+    }
+
+    return Container(
+      color: backgroundColor,
+      child: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: children,
+          )),
+    );
+  }
+}
