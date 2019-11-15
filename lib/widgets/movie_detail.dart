94d15e3ea07fc6a921248dc0c94e440cc8463fdc
retrychx <retrychx@163.com>
2019-11-15 17:35:47 +0800

    star

--- lib/widgets/movie_detail.dart
+++ lib/widgets/movie_detail.dart
@@ -1,6 +1,10 @@
-// import 'package:flutter/material.dart"
+import 'package:flutter/material.dart';
+import 'package:douban/widgets/rating_star.dart';
+import 'package:douban/utils/hex_color.dart';
 
-// class MovieDetail extends StatelessWidget {
-//   final bool isShowStar;
-//   MovieDetail(@required )
-// }
+class MovieDetail extends StatelessWidget {
+  final bool isShowStar;
+  final Color hotColor = HexColor("#F2A325");
+  final Color soonColor = HexColor("#E8E9EA");
+  MovieDetail(@required )
+}
