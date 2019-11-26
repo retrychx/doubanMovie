cea7d1b5ef339e3ecac69594b9bfd0dd5e30c250
常红旭 <migor@changhongxudeMacBook-Pro.local>
2019-11-26 23:01:52 +0800

    设置

--- lib/widgets/list_tile.dart
+++ lib/widgets/list_tile.dart
@@ -1,13 +1,40 @@
 import "package:flutter/material.dart";
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ListTile extends StatelessWidget {
   final String title;
   final double paddingLeft;
   final double paddingRight;
   final Image image;
+  final double height;
 
-  ListTile({this.title, this.paddingLeft, this.paddingRight, this.image});
+  ListTile(
+      {this.title,
+      this.paddingLeft,
+      this.paddingRight,
+      this.image,
+      this.height});
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      height: ScreenUtil().setHeight(this.height),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding:
+                EdgeInsets.only(left: ScreenUtil().setWidth(this.paddingLeft)),
+          ),
+          Text(this.title),
+          Image.asset("images/ic_back.png"),
+          Padding(
+            padding: EdgeInsets.only(
+                right: ScreenUtil().setWidth(this.paddingRight)),
+          )
+        ],
+      ),
+    );
+  }
 }
